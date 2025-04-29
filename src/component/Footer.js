d90291ad49131f60288ccd33ffa481690c404d4b
phumudzo03 <tshivhulas.p3@gmail.com>
2025-04-29 19:33:56 +0200

    added init files

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -38,7 +38,7 @@ function Footer() {
 
         {/* Right Section: Contact Details */}
         <div className="footer-contact">
-          <p><FaPhone /> +27 71 234 5678</p>
+          <p><FaPhone /> +2772 201 4044</p>
           <p><FaEnvelope /> info@sesra.co.za</p>
           <p><FaMapMarkerAlt /> Stand No 69, Ga Mothapo, Polokwane 0734, South Africa</p>
         </div>
